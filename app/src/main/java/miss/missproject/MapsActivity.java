9a1e356598b3a30f245b102225fa701e23a97ac0
lazar81ba <lazar81ba@gmail.com>
2017-12-18 16:14:01 +0100

    "Sledzenie pozycji"

--- app/src/main/java/miss/missproject/MapsActivity.java
+++ app/src/main/java/miss/missproject/MapsActivity.java
@@ -1,24 +1,41 @@
 package miss.missproject;
 
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Button;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class MapsActivity extends AppCompatActivity
@@ -26,15 +43,20 @@ public class MapsActivity extends AppCompatActivity
         OnMyLocationButtonClickListener,
         OnMyLocationClickListener,
         OnMapReadyCallback,
-        ActivityCompat.OnRequestPermissionsResultCallback {
+        ActivityCompat.OnRequestPermissionsResultCallback,
+        LocationListener {
 
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
+    private boolean shouldAddCoordinates = false;
+    private List<LatLng> coordinates = new LinkedList<>();
 
     private boolean mPermissionDenied = false;
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    String locationProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +69,36 @@ public class MapsActivity extends AppCompatActivity
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
         setSupportActionBar(myToolbar);
-
+        //Back button on toolbar
         myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
+
+        this.initializeLocationManager();
+    }
+
+    private void initializeLocationManager() {
+
+        //get the location manager
+        this.locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        this.locationProvider = LocationManager.GPS_PROVIDER;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    2000,
+                    10, this);
+            Location location = locationManager.getLastKnownLocation(locationProvider);
+            //initialize the location
+            if (location != null) {
+
+              //  onLocationChanged(location);
+            }
+        }
 
 
     }
@@ -84,7 +129,7 @@ public class MapsActivity extends AppCompatActivity
 
     @Override
     public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "Searching for current location" , Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Searching for current location", Toast.LENGTH_SHORT).show();
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
@@ -130,8 +175,74 @@ public class MapsActivity extends AppCompatActivity
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 
-   public void goToMenu(View view){
-       Intent aboutScreen = new Intent(this, MainActivity.class);
-       this.startActivity(aboutScreen);
-   }
+    public void goToMenu(View view) {
+        Intent aboutScreen = new Intent(this, MainActivity.class);
+        this.startActivity(aboutScreen);
+    }
+
+    public void beginTracking(View view) {
+        shouldAddCoordinates = shouldAddCoordinates ? false : true;
+        String buttonText = ((Button) view).getText().toString();
+        buttonText = buttonText.equals("TRACK") ? "STOP TRACKING" : "TRACK";
+        ((Button) view).setText(buttonText);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (shouldAddCoordinates) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Location location = lm.getLastKnownLocation(locationProvider);
+                if (location != null) {
+                    LatLng coordinate = new LatLng(location.getLatitude(), location.getLongitude());
+                    coordinates.add(coordinate);
+                }
+            }
+
+        } else {
+            coordinates.clear();
+            mMap.clear();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i("called", "onLocationChanged");
+        //when the location changes, update the map by zooming to the location
+        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(),location.getLongitude()));
+        this.mMap.moveCamera(center);
+
+        CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
+        this.mMap.animateCamera(zoom);
+        if(shouldAddCoordinates){
+            LatLng coordinate = new LatLng(location.getLatitude(),location.getLongitude());
+            coordinates.add(coordinate);
+            Polyline line = mMap.addPolyline(new PolylineOptions()
+                    .addAll(coordinates)
+                    .width(5)
+                    .color(Color.BLUE));
+        }
+    }
+
+    @Override
+    public void onProviderDisabled(String arg0) {
+
+        Log.i("called", "onProviderDisabled");
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String arg0) {
+
+        Log.i("called", "onProviderEnabled");
+
+        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+
+        Log.i("called", "onStatusChanged");
+    }
 }
